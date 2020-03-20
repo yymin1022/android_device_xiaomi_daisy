215c736ef94edfa37e11b2e49c2d0a035d70606b
a-huk <56552991+a-huk@users.noreply.github.com>
2020-03-20 19:06:55 +0100

    Update Android.mk

--- thermal/Android.mk
+++ thermal/Android.mk
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
 #LOCAL_MODULE := thermal.$(TARGET_BOARD_PLATFORM)
 #LOCAL_MODULE_RELATIVE_PATH := hw
@@ -29,4 +29,4 @@ include $(CLEAR_VARS)
 #LOCAL_MODULE_TAGS := optional
 #LOCAL_CFLAGS := -Wno-unused-parameter
 
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
