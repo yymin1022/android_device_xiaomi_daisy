a4ea5cad543a11d3e1af4b18605915f66159ddc1
a-huk <56552991+a-huk@users.noreply.github.com>
2020-03-20 19:01:52 +0100

    Update Android.mk

--- thermal/Android.mk
+++ thermal/Android.mk
@@ -20,13 +20,13 @@ include $(CLEAR_VARS)
 #LOCAL_MODULE_RELATIVE_PATH := hw
 #LOCAL_PROPRIETARY_MODULE := true
 
-LOCAL_SRC_FILES := thermal.c
-LOCAL_SRC_FILES += thermal_common.c
-LOCAL_SRC_FILES += thermal_target.c
+#LOCAL_SRC_FILES := thermal.c
+#LOCAL_SRC_FILES += thermal_common.c
+#LOCAL_SRC_FILES += thermal_target.c
 
-LOCAL_HEADER_LIBRARIES := libutils_headers libhardware_headers
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -Wno-unused-parameter
+#LOCAL_HEADER_LIBRARIES := libutils_headers libhardware_headers
+#LOCAL_SHARED_LIBRARIES := liblog libcutils
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
