440312c11ee1f84264d860e6b3160044056dbac5
a-huk <56552991+a-huk@users.noreply.github.com>
2020-04-03 18:30:30 +0200

    Transfer to lineage tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,6 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/potato_daisy.mk
+    $(LOCAL_DIR)/lineage_daisy.mk
+COMMON_LUNCH_CHOICES := \
+    lineage_daisy-userdebug
