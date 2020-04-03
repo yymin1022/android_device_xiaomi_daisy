407b5862f4401b9e35f0148c51a65334c6684f65
a-huk <56552991+a-huk@users.noreply.github.com>
2020-04-03 18:35:36 +0200

    Rename branch, based of croquette 03/04/20

--- device.mk
+++ device.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+
 $(call inherit-product, vendor/xiaomi/daisy/daisy-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
