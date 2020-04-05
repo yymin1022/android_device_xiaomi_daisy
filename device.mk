45f11a13f0d724d0d0640c5a7df1bed7623f35b6
a-huk <56552991+a-huk@users.noreply.github.com>
2020-04-05 16:00:40 +0200

    Possible LED fix

--- device.mk
+++ device.mk
@@ -30,7 +30,9 @@ PRODUCT_USES_QCOM_HARDWARE := true
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+     $(LOCAL_PATH)/overlay \
+     $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
